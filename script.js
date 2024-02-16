187b219e7fa571bf4d6b50b1683b1735ceabaaaa
Tracy-miranja <barazatracy44@gmail.com>
2024-02-16 18:52:09 +0300

    Add interactivity

--- script.js
+++ script.js
@@ -19,3 +19,79 @@ pageTurnBtn.forEach((el, index) => {
     }
   };
 });
+
+//contact me button
+
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactMeBtn = document.querySelector(".btn.contact-me");
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+  pageNumber--;
+
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+// back profile button
+
+const backProfileBtns = document.querySelectorAll(".back-profile");
+
+backProfileBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    pages.forEach((_, index) => {
+      setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove("turn");
+        setTimeout(() => {
+          reverseIndex();
+          pages[pageNumber].style.zIndex = 10 + index;
+        });
+      }, (index + 1) * 200 + 100);
+    });
+  });
+});
+
+//opening animation
+const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector(".book-page.page-left");
+
+//opening animation(cover)
+setTimeout(() => {
+  coverRight.classList.add("turn");
+}, 2100);
+
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+//opening animation(all pages cover)
+
+pages.forEach((_, index) => {
+  setTimeout(() => {
+    reverseIndex();
+    pages[pageNumber].classList.remove("turn");
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].style.zIndex = 10 + index;
+    });
+  }, (index + 1) * 200 + 2100);
+});
